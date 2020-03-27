540cbd1abed29e83d08d241f0e4b451125646508
snakeguy2000 <snakeguy2000@gmail.com>
2020-03-27 14:58:07 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -36,6 +36,20 @@ class Main extends Component {
         />
       );
     };
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              campsite => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
     return (
       <div>
         <Header />
@@ -47,15 +61,9 @@ class Main extends Component {
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Redirect to="/home" />
         </Switch>
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              campsite => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
         <Footer />
       </div>
     );
