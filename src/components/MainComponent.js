c4db4fa2f176559671c65cd5f2aafeb487f4474f
snakeguy2000 <snakeguy2000@gmail.com>
2020-03-27 14:50:47 -0700

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Contact from './ContactComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: CAMPSITES
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          campsite={
+            this.state.campsites.filter(campsite => campsite.featured)[0]
+          }
+          promotion={
+            this.state.promotions.filter(promotion => promotion.featured)[0]
+          }
+          partner={this.state.partners.filter(partner => partner.featured)[0]}
+        />
+      );
     };
-
     return (
       <div>
         <Header />
         <Switch>
+          <Route exact path="/contactus" component={Contact} />
           <Route path="/home" component={HomePage} />
           <Route
             exact
